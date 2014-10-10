0d283b473eb197b766ea96fe9ab456cc68483ba5
marciandmnd <marciandmnd@yahoo.com>
2014-10-10 15:45:34 -0700

    commit

--- assets/py/forecast.bat
+++
@@ -1,5 +0,0 @@
-copy C:\Upper_Guad_RAS_Model\ToGIS.xls C:\Apache2.2\htdocs\scvwd\app_beta\assets\py
-C:\Python27\python.exe C:\Apache2.2\htdocs\scvwd\app_beta\assets\py\excel_to_JS.py
-timeout /t 5
-pause
-
