57d0901aa2fca553da0d1ebfeeb633b9de2bd897
marciandmnd <marciandmnd@yahoo.com>
2015-03-16 13:21:31 -0700

    added admin.php

---
+++ admin.php
@@ -0,0 +1,13 @@
+<?php 
+	exec("java -jar file.jar arguments", $output); 
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+	<body>
+		<form action=""
+		<button id="runJavaTest">test</button>
+	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	</body>	
+</html>
