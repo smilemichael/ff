511e4283935cd696a58b6c13860d9f15807bfab8
root <root@ip-172-31-51-120.ec2.internal>
2015-06-02 12:24:52 -0700

    initial commtit frome AWS EC2. Removed db creds from php assets."

--- admin.php
+++ admin.php
@@ -1,13 +1,60 @@
 <?php 
-	exec("java -jar file.jar arguments", $output); 
+	// if($_SERVER['REQUEST_METHOD'] === 'POST'){
+	// 	exec("java -version" ,$output);
+	// 	echo $output; 
+	// }
+	session_start();
+	if($_SESSION['login'] != 1){
+		header("Location: admin_login.php"); /* Redirect browser */
+		exit();
+	}
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta name="author" content="Marcian Diamond">
+		<title>SCVWD Flood Admin Page</title>
+		<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+		<style type="text/css">
+			body{
+				background-color: #FFFFCC;
+			}
+			#mainConsole{
+				width: 80%;
+				min-width: 600px;
+				background-color: #66FFFF;
+				margin: 0 auto;
+			}
+		</style>
 	</head>
 	<body>
-		<form action=""
-		<button id="runJavaTest">test</button>
-	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+		<nav class="navbar navbar-default">
+  			<div class="container-fluid">
+    				<div class="navbar-header">
+						<a class="navbar-brand" href="#">
+						SCVWD Flood Admin - Dashboard
+      					</a>
+    				</div>
+    				<ul class="nav navbar-nav navbar-right">
+        				<li><a href="logout.php">Logout</a></li>
+  					</ul>
+		</nav>
+		<div id='mainConsole'>
+			<h2>Forecasted Flood Conditions</h2>
+			<table class="table table-hover">
+				<tr>
+					<th>Station Name</th>
+					<th>Flood Condition Code</th>
+					<th>Actions</th>
+				<tr>
+					<td>Ross Ck at Cherry Ave.</td>
+				</tr>
+			</table>
+			<form method="post" action="admin.php"> 
+				<button id="runJavaTest">test</button>
+			</form>
+		</div>
+		<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 	</body>	
 </html>
